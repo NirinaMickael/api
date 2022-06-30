9e2c06ba35161db82b29dcd870c4bf9116a1f1e0
NirinaMickael <todisoanirinamickael@gamil.com>
2022-06-30 15:10:06 +0300

    api

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,16 @@ import cors from "cors"
 import userRoute from "./routes/user.route";
 import ConvRoute from "./routes/conversation.route";
 import msgRoute from "./routes/message.route";
+import { createServer } from "http";
+import { Server } from "socket.io";
 const app: Application = express();  
-
+const httpServer = createServer(app);
+const io = new Server(httpServer,{
+	cors: {
+	  origin: "*",
+	  methods: ["GET", "POST"]
+	}}
+);
 // variable database
 const port = process.env.port as unknown as number;
 const portDb = process.env.portDb as unknown as number;
@@ -25,12 +33,25 @@ app.use("/api",userRoute);
 app.use("/message",msgRoute)
 app.use("/conversation",ConvRoute)
 try {
-	app.listen(port, (): void => {
+	httpServer.listen(port, (): void => {
 		console	.log(`Connected successfully on port ${port}`);
 		console.log(dbName)
 		connect(dbName,portDb,hostDb)
 		// data connection
 	});
+	io.on('connection',(socket)=>{
+		socket.on('joinRoom',roomId=>{;
+			socket.join(roomId);
+		})
+		socket.on('sendMessage',buffer=>{
+			socket.join(buffer["data"].conversationId);
+			// io.to(buffer["data"].conversationId).emit('received',buffer["data"].messages);
+			socket.broadcast.emit('received',buffer);
+		})
+		socket.on('entrainEcrire',data=>{
+			socket.broadcast.emit('writting',data);
+		})
+	})
 } catch (error : any) {
 	console.error(`Error occured: ${error.message}`);
 }
